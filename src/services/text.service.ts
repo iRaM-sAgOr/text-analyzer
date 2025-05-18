b8ebb4209fd30c6e2a9f4459e15838701230b27a
ikramul.hoque <ikramul.hoque@bjitgroup.com>
2025-05-18 23:57:44 +0600

    Write service class methods with analyzer utils

--- src/services/text.service.ts
+++ src/services/text.service.ts
@@ -10,9 +10,9 @@ export class TextService {
         this.textRepository = textRepository;
     }
 
-    async createText(dto: {content: string},userId: string): Promise<IText> {
+    async createText(dto: { content: string }, userId: string): Promise<IText> {
         createTextSchema.parse(dto);
-        const text:IText = {
+        const text: IText = {
             content: dto.content,
             userId,
             createdAt: new Date(),
@@ -30,4 +30,83 @@ export class TextService {
         const count = TextAnalyzer.countWords(text.content);
         return count;
     }
+
+    async getCharacterCount(id: string, userId: string, countWhitespace: boolean = false): Promise<number> {
+        const text = await this.textRepository.getTextById(id);
+        if (!text || text.userId !== userId) {
+            throw new Error("Text not found or user not authorized");
+        }
+
+        const count = TextAnalyzer.countCharacters(text.content, countWhitespace);
+        return count;
+    }
+
+    async getSentenceCount(id: string, userId: string): Promise<number> {
+        const text = await this.textRepository.getTextById(id);
+        if (!text || text.userId !== userId) {
+            throw new Error("Text not found or user not authorized");
+        }
+
+        const count = TextAnalyzer.countSentences(text.content);
+        return count;
+    }
+
+    async getParagraphCount(id: string, userId: string): Promise<number> {
+        const text = await this.textRepository.getTextById(id);
+        if (!text || text.userId !== userId) {
+            throw new Error("Text not found or user not authorized");
+        }
+
+        const count = TextAnalyzer.countParagraphs(text.content);
+        return count;
+    }
+
+    async getLongestWords(id: string, userId: string, returnAll: boolean = false): Promise<string | string[]> {
+        const text = await this.textRepository.getTextById(id);
+        if (!text || text.userId !== userId) {
+            throw new Error("Text not found or user not authorized");
+        }
+
+        const longestWords = TextAnalyzer.findLongestWords(text.content, returnAll);
+        return longestWords;
+    }
+
+    async updateText(id: string, dto: { content: string }, userId: string): Promise<IText> {
+        createTextSchema.parse(dto);
+        const text = await this.textRepository.getTextById(id);
+        if (!text || text.userId !== userId) {
+            throw new Error("Text not found or user not authorized");
+        }
+
+        const updatedText: IText = {
+            ...text,
+            content: dto.content,
+            updatedAt: new Date(),
+        }
+        const result = await this.textRepository.updateText(id, updatedText);
+        if (!result) {
+            throw new Error(`Text with id ${id} not found`);
+        }
+        return result;
+    }
+
+    async deleteText(id: string, userId: string): Promise<boolean> {
+        const text = await this.textRepository.getTextById(id);
+        if (!text || text.userId !== userId) {
+            throw new Error("Text not found or user not authorized");
+        }
+
+        const result = await this.textRepository.deleteText(id);
+        return result;
+    }
+
+    async getTextByUserId(userId: string): Promise<IText[]> {
+        const texts = await this.textRepository.getTextByUserId(userId);
+        if (!texts) {
+            throw new Error("No texts found for this user");
+        }
+        return texts;
+    }
+
+
 }
